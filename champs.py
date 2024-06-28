59cfb3378fe68fde5200c5018a6e53500010d12b
Gabriel Hardoim <37919492+ghardoim@users.noreply.github.com>
2024-06-28 17:46:25 -0300

    creating some charts

--- champs.py
+++ champs.py
@@ -7,7 +7,14 @@ st.set_page_config(page_title="Bongas", layout="centered")
 st.write(f"<h1 {csscenter}>Yugioh Championship<h1>", unsafe_allow_html=True)
 
 st.dataframe(pd.read_csv("total.csv"), hide_index=True)
-st.dataframe(pd.read_csv("points.csv"), hide_index=True)
+
+pointsdf = pd.read_csv("points.csv")
+pointsdf["round"] = pointsdf["round"].apply(lambda x: f"Round {x}")
+
+for column in pointsdf.columns.drop(["player", "round", "matches", "tags"]):
+    st.line_chart(pointsdf, x="round", y=column, color="player")
+
+# st.dataframe(pd.read_csv("points.csv"), hide_index=True)
 
 st.write(f"<h4 {csscenter}>History of Plays<h4>", unsafe_allow_html=True)
 with st.container(height=500):
